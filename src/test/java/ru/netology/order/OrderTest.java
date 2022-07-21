fca3b463d8d7d61c16758334cbab51dfd6971c16
Roman Khudenko <romanhudenko370@gmail.com>
2022-07-21 12:07:49 +0400

    Fixed positive selectors, added browser reuse, fixed negative test cases, fixed tests to use test marks, reformatted code

--- src/test/java/ru/netology/order/OrderTest.java
+++ src/test/java/ru/netology/order/OrderTest.java
@@ -1,5 +1,7 @@
 package ru.netology.order;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -7,73 +9,93 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class OrderTest {
+    @BeforeEach
+    public void openBrowser() {
+        open("http://localhost:9999/");
+    }
+
     @Test
     public void positive() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Иванов Иван");
         $(By.name("phone")).setValue("+79991234567");
         $(By.className("checkbox__box")).click();
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.className("Success_successBlock__2L3Cw")).shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(By.cssSelector(".paragraph[data-test-id=\"order-success\"]")).shouldHave(
+                text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")
+        );
     }
 
     @Test
     public void positiveNameMinusSign() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Иванов-Иван");
         $(By.name("phone")).setValue("+79991234567");
         $(By.className("checkbox__box")).click();
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.className("Success_successBlock__2L3Cw")).shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(By.cssSelector(".paragraph[data-test-id=\"order-success\"]")).shouldHave(
+                text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")
+        );
     }
 
     @Test
     public void wrongName() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Qwerty");
+        $(By.name("phone")).setValue("+79991234567");
+        $(By.className("checkbox__box")).click();
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.cssSelector("span[data-test-id=\"name\"] span.input__sub")).shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(By.cssSelector("[data-test-id=\"name\"] .input__sub")).shouldHave(
+                text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")
+        );
     }
 
     @Test
     public void emptyName() {
-        open("http://localhost:9999/");
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.cssSelector("span[data-test-id=\"name\"] span.input__sub")).shouldHave(text("Поле обязательно для заполнения"));
+        $(By.name("phone")).setValue("+79991234567");
+        $(By.className("checkbox__box")).click();
+        $(By.cssSelector("[data-test-id=\"name\"] .input__sub")).shouldHave(
+                text("Поле обязательно для заполнения")
+        );
     }
 
     @Test
     public void wrongPhone() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Иванов Иван");
         $(By.name("phone")).setValue("wrong");
+        $(By.className("checkbox__box")).click();
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.cssSelector("span[data-test-id=\"phone\"] span.input__sub")).shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).shouldHave(
+                text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")
+        );
     }
 
     @Test
     public void missPlacedPlusSign() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Иванов Иван");
         $(By.name("phone")).setValue("7+9991234567");
+        $(By.className("checkbox__box")).click();
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.cssSelector("span[data-test-id=\"phone\"] span.input__sub")).shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).shouldHave(
+                text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")
+        );
     }
 
     @Test
     public void emptyPhone() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Иванов Иван");
+        $(By.className("checkbox__box")).click();
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.cssSelector("span[data-test-id=\"phone\"] span.input__sub")).shouldHave(text("Поле обязательно для заполнения"));
+        $(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).shouldHave(
+                text("Поле обязательно для заполнения")
+        );
     }
 
     @Test
     public void noAgreement() {
-        open("http://localhost:9999/");
         $(By.name("name")).setValue("Иванов Иван");
         $(By.name("phone")).setValue("+79991234567");
         $(By.cssSelector("button[type=\"button\"]")).click();
-        $(By.cssSelector("label[data-test-id=\"agreement\"]")).shouldHave(cssClass("input_invalid"));
+        $(By.cssSelector("[data-test-id=\"agreement\"]")).shouldHave(
+                cssClass("input_invalid")
+        );
     }
 }
